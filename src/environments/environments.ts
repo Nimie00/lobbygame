8f39d51c473907ddd9eff4665f0e06eaa3950752
Muhel Nimród Róbert <h267734@stud.u-szeged.hu>
2025-05-24 19:57:30 +0200

    Környezet

---
+++ src/environments/environments.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBHsS6AnMFA_YglFg5HXwY1C2MZepxsYBQ",
+  authDomain: "nimie-game-lobby.firebaseapp.com",
+  projectId: "nimie-game-lobby",
+  storageBucket: "nimie-game-lobby.appspot.com",
+  messagingSenderId: "269405095017",
+  appId: "1:269405095017:web:a6a5be9170ea54d7b6cef8",
+  measurementId: "G-YLWEHT7Z23"
+};
